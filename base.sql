8600f3149b3f9fdfc19980161bbe4a70c933ed0b
Григорий <grinzya@yandex.ru>
2021-02-01 01:36:49 +0500

    Create base.sql
    Добавлен пример базы данных

---
+++ base.sql
@@ -0,0 +1,47 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `user_table` (
+  `id` int(11) NOT NULL,
+  `login` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `value_table` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_login` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `temperature` float DEFAULT NULL,
+  `time_value` datetime NOT NULL DEFAULT current_timestamp(),
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `humidity` float DEFAULT NULL,
+  `pressure` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `value_table`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `user_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `value_table`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
